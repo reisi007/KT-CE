8ae278cad61de2edc3578f2b23b8bf79c2751074
Florian Reisinger <reisi007@gmail.com>
2016-04-14 09:05:13 +0200

    Save MySQL data outside of container

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ echo Stopping all running containers
 sudo docker stop kt-apache kt-mysql
 sudo docker rm kt-apache kt-mysql
 echo Start MySQL
-sudo docker run --name kt-mysql -e MYSQL_ROOT_PASSWORD=1234 -d mysql/mysql-server:latest
+sudo docker run --name kt-mysql -v /dockerData/kt-mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 -d mysql/mysql-server:latest
 echo Start Apache
 sudo docker run -p 80:80 --name=kt-apache --link kt-mysql:mysql -d kt-apache
 echo 

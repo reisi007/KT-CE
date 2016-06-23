5142a42a1382dcf863fed812d710b254cb3f8233
Florian Reisinger <reisi007@gmail.com>
2016-06-23 19:07:22 +0200

    Deleted primary key, added index

--- apache_app/app/create.sql
+++ apache_app/app/create.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS ktce;
 DROP TABLE IF EXISTS ktce.main;
 CREATE TABLE ktce.main (
-  time TIMESTAMP PRIMARY KEY,
+  time TIMESTAMP,
   data VARCHAR(1024)
 );
+CREATE INDEX pi on ktce.main (time) using btree
